9f66d1ffb0d0f548b4a339b3cf750bc36659f547
i3games <straeubig@gmail.com>
2018-02-24 17:56:56 +0000

    no more page reloads

--- js/app.js
+++ js/app.js
@@ -5,40 +5,28 @@ var folders = [];
 var gui = new dat.GUI();
 var scene = document.querySelector('a-scene');
 
+function run() {
+  controller.onFinishChange(function (selected) {
+    var folder = selected.substring(1); // workaround for dat.GUI bug sorting strings
+    localStorage.setItem("folder", folder);
+    document.getElementById('panel1').setAttribute('src', `assets/${folder}/panel1.png`);
+    document.getElementById('panel2').setAttribute('src', `assets/${folder}/panel2.png`);
+    document.getElementById('panel3').setAttribute('src', `assets/${folder}/panel3.png`);
+    document.getElementById('panel4').setAttribute('src', `assets/${folder}/panel4.png`);
+    document.getElementById('panel5').setAttribute('src', `assets/${folder}/panel5.png`);
+    document.getElementById('panel6').setAttribute('src', `assets/${folder}/panel6.png`);
+  });
+}
+
 for (i = 1; i < 50; i++) {
   folders.push(i < 10 ? " 0" + i : " " + i); // the leading space is a workaround for a dat.GUI bug sorting strings
 }
 
 controller = gui.add(datGuiFolderElement, 'folder', folders);
-
 folder = localStorage.getItem("folder");
 if (folder == null) {
   folder = "01"; // default
 }
 controller.setValue(folder);
 
-document.getElementById('p1').setAttribute('src', `assets/${folder}/panel1.png`);
-document.getElementById('p2').setAttribute('src', `assets/${folder}/panel2.png`);
-document.getElementById('p3').setAttribute('src', `assets/${folder}/panel3.png`);
-document.getElementById('p4').setAttribute('src', `assets/${folder}/panel4.png`);
-document.getElementById('p5').setAttribute('src', `assets/${folder}/panel5.png`);
-document.getElementById('p6').setAttribute('src', `assets/${folder}/panel6.png`);
-
-controller.onFinishChange(function (selected) {
-  var folder = selected.substring(1); // workaround for dat.GUI bug sorting strings
-  localStorage.setItem("folder", folder);
-  location.reload(); // set the folder number and reload the page
-})
-
-// this all has to run before scene.hasLoaded
-// hacky, all this
-// not nice
-
-if (scene.hasLoaded === true) {
-  run();
-} else {
-  scene.addEventListener('loaded', run);
-}
-
-function run() {
-}
+if (scene.hasLoaded === true) { run(); } else { scene.addEventListener('loaded', run); }
