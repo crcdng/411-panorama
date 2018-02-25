b01c562e96953169b851b87749913ad090b4dd49
i3games <straeubig@gmail.com>
2018-02-25 18:22:34 +0000

    single quotes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // ES5 for older phones
 var controller, i;
-var datGuiFolderElement = { folder: " 00" }; // dat.GUI needs an object
+var datGuiFolderElement = { folder: ' 00' }; // dat.GUI needs an object
 var folders = [];
 var gui = new dat.GUI();
 var scene = document.querySelector('a-scene');
@@ -8,7 +8,7 @@ var scene = document.querySelector('a-scene');
 function run() {
   controller.onFinishChange(function (selected) {
     var folder = selected.substring(1); // workaround for dat.GUI bug sorting strings
-    localStorage.setItem("folder", folder);
+    localStorage.setItem('folder', folder);
     document.getElementById('panel1').setAttribute('src', `assets/${folder}/panel1.png`);
     document.getElementById('panel2').setAttribute('src', `assets/${folder}/panel2.png`);
     document.getElementById('panel3').setAttribute('src', `assets/${folder}/panel3.png`);
@@ -19,13 +19,13 @@ function run() {
 }
 
 for (i = 1; i < 50; i++) {
-  folders.push(i < 10 ? " 0" + i : " " + i); // the leading space is a workaround for a dat.GUI bug sorting strings
+  folders.push(i < 10 ? ' 0' + i : ' ' + i); // the leading space is a workaround for a dat.GUI bug sorting strings
 }
 
 controller = gui.add(datGuiFolderElement, 'folder', folders);
-folder = localStorage.getItem("folder");
+folder = localStorage.getItem('folder');
 if (folder == null) {
-  folder = "01"; // default
+  folder = '01'; // default
 }
 controller.setValue(folder);
 
